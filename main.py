5aeaf18684cdb2306665429d5987587b6bd90097
Shin0me <shin0n0me930506@gmail.com>
2024-12-25 20:46:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pydub import AudioSegment
-from flask import Flask,request,Response
+from flask import Flask,request,Response,send_file
 from json import load
 
 
@@ -7,16 +7,19 @@ def japaneseToMorseSound_converter(message:str) -> str:
     exportedMorseSignalsPath:str = "converted_morse_signals.mp3"
     morseAudio:object = None
     morseTable:dict = {}
-
+    
+    print("loading morse signal assets")
     morse_long = AudioSegment.from_mp3("morse_long.mp3")
     morse_short = AudioSegment.from_mp3("morse_short.mp3")
     morseSignals:list = [morse_short,morse_long]
     morse_space = AudioSegment.from_mp3("morse_space.mp3")
-
+    
+    print("loading japanese-morse conversion table")
     with open("japanese-morse table.json","r") as f:
         morseTable = load(f)
 
-    for letter in message:
+    for index,letter in enumerate(message):
+        print(f"Now generating morsw signals for {letter}({index+1}/{len(message)})")
         for signal in morseTable.get(letter,[]):
 
             if len(signal) > 0:
@@ -38,7 +41,7 @@ def main():
     morseAudioPath:str = japaneseToMorseSound_converter(requestMessage)
 
 
-    return f"message is {requestMessage}"
+    return send_file(morseAudioPath, as_attachment=False)
 
 
 if __name__ == "__main__":
