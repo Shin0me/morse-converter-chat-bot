560c6067d1bb98e25615acd1b77f1057e664688f
Shin0me <shin0n0me930506@gmail.com>
2024-12-25 20:36:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,31 +2,40 @@ from pydub import AudioSegment
 from flask import Flask,request,Response
 from json import load
 
-app:object = Flask(__name__)
 
 def japaneseToMorseSound_converter(message:str) -> str:
+    exportedMorseSignalsPath:str = "converted_morse_signals.mp3"
+    morseAudio:object = None
+    morseTable:dict = {}
+
     morse_long = AudioSegment.from_mp3("morse_long.mp3")
     morse_short = AudioSegment.from_mp3("morse_short.mp3")
-    morseSounds:list = [morse_short,morse_long]
+    morseSignals:list = [morse_short,morse_long]
     morse_space = AudioSegment.from_mp3("morse_space.mp3")
-    convertedFile:object = None
-    morseTable:dict = {}
 
     with open("japanese-morse table.json","r") as f:
         morseTable = load(f)
 
-
     for letter in message:
-        for signal in morseTable.get(letter,""):
-            pass
+        for signal in morseTable.get(letter,[]):
+
+            if len(signal) > 0:
+                morseAudio + = morseSignals[signal]
+            else:
+                break
+        morseAudio += morse_space
+
+    morseAudio.export(exportedMorseSignalsPath,format="mp3")
+    return exportedMorseSignalsPath
 
 
+app:object = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def main():
     requestMessage:str = str(request.args.get("message"))
     print(requestMessage)
-    japaneseToMorseSound_converter(requestMessage)
+    morseAudioPath:str = japaneseToMorseSound_converter(requestMessage)
 
 
     return f"message is {requestMessage}"
