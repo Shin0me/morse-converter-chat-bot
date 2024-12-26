36477e4690465325ebdb5020f3654beaa9f1d2cf
Shin0me <shin0n0me930506@gmail.com>
2024-12-26 13:28:32 +0900

    some data

--- main.py
+++ main.py
@@ -1,47 +1,92 @@
 from pydub import AudioSegment
 from flask import Flask,request,Response,send_file
 from json import load
+from packages.LineAPI import LineAPI
+
+from sys import exit
+from time import sleep
+
+from os import environ,getcwd
+from os.path import join
+from dotenv import load_dotenv
 
 
-def japaneseToMorseSound_converter(message:str) -> str:
+
+def load_env() -> str:
+    load_dotenv()
+    channelAccessToken:str = environ["channelAccessToken"]
+
+    return channelAccessToken
+
+
+def japaneseToMorseSound_converter(message:str) -> tuple[str,int]:
     exportedMorseSignalsPath:str = "converted_morse_signals.mp3"
-    morseAudio:object = None
+    morseAudio:object = AudioSegment.silent(duration=0)
     morseTable:dict = {}
-    
+    singleUnitDuration:int =  60 # *0.001 sec
+
     print("loading morse signal assets")
-    morse_long = AudioSegment.from_mp3("morse_long.mp3")
-    morse_short = AudioSegment.from_mp3("morse_short.mp3")
+    morse_long:object = AudioSegment.from_file("morse_long.mp3") #!   0.18 sec
+    morse_short:object = AudioSegment.from_file("morse_short.mp3")#!  0.06 sec
     morseSignals:list = [morse_short,morse_long]
-    morse_space = AudioSegment.from_mp3("morse_space.mp3")
-    
+    morse_space:object = AudioSegment.silent(duration=singleUnitDuration) #! 0.06 sec
+
     print("loading japanese-morse conversion table")
-    with open("japanese-morse table.json","r") as f:
+    with open("japanese-morse_table.json","r",encoding="utf-8") as f:
         morseTable = load(f)
 
+    if not message:
+        print("text should be written in the message!!")
+        return
+
+    print(f"requestedMessage:{message}")
+
     for index,letter in enumerate(message):
-        print(f"Now generating morsw signals for {letter}({index+1}/{len(message)})")
-        for signal in morseTable.get(letter,[]):
+        morseList:list  =morseTable.get(letter,[])
+        if len(morseList) == 0:
+            return
 
-            if len(signal) > 0:
-                morseAudio + = morseSignals[signal]
-            else:
+        print(f"Now generating morse signals for {letter}({index+1}/{len(message)})")
+        for signal in morseList:
+            try:
+                morseAudio += morseSignals[signal]
+            except Exception as e:
+                print(f"interruption occured. Error:{e}")
                 break
-        morseAudio += morse_space
+            morseAudio += morse_space
 
+    audioDuration:int = len(morseAudio)
     morseAudio.export(exportedMorseSignalsPath,format="mp3")
-    return exportedMorseSignalsPath
+    return exportedMorseSignalsPath,audioDuration
+
 
 
 app:object = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def main():
-    requestMessage:str = str(request.args.get("message"))
-    print(requestMessage)
-    morseAudioPath:str = japaneseToMorseSound_converter(requestMessage)
+
+    if request.method == "POST":
+        line:object = LineAPI()
+        message,messageType,contentProvider = line.parse_POSTrequest(request.get_json())
+        morseAudioPath,audioDuration = japaneseToMorseSound_converter(message.strip())
+        absoluteAudioPath:str = join(getcwd(),morseAudioPath)
+
+        channelAccessToken:str = load_env()
+        line.addCredentials(channelAccessToken)
+        line.addMessage_forReply(messageType="audio",contentURL=absoluteAudioPath,audioDuration=audioDuration)
+        response:object = line.sendMessage()
 
 
-    return send_file(morseAudioPath, as_attachment=False)
+    else:
+        pass
+        """ requestMessage:str = str(request.args.get("message"," "))
+        morseAudioPath,audioDuration = japaneseToMorseSound_converter(requestMessage.strip())
+        absoluteAudioPath:str = join(getcwd(),morseAudioPath)
+        print("This method is not allowed. POST is only accessible.")
+        return send_file(absoluteAudioPath)"""
+
+    return Response(status=200)
 
 
 if __name__ == "__main__":
---
+++ packages/LineAPI.py
@@ -0,0 +1,63 @@
+import requests as req
+
+replyUrl:str = "https://api.line.me/v2/bot/message/reply"
+
+class LineAPI:
+
+    def __init__(self):
+        pass
+
+    def parse_POSTrequest(self,requestData:dict) -> tuple[str,str,str]:
+
+        event:dict = requestData.get("events",[{}])[0]
+
+        contentProvider:str = event["message"].get("contentProvider","").get("type","")
+        message:str = event["message"].get("text","None").strip()
+        messageType:str = event.get("type","")
+        self.replyToken:str  = event.get("replyToken","None")
+
+        self.body:dict = {
+            "replyToken":self.replyToken,
+        }
+
+        return message,messageType,contentProvider
+
+    def fetchRecievedFiles(self):
+        pass
+
+
+    def addCredentials(self,channelAccessToken:str) -> None:
+        self.channelAccessToken = channelAccessToken
+
+        self.header = {
+        "Content-Type":"application/json",
+        "Authorization":"Bearer " + self.channelAccessToken,
+        }
+
+
+    def addMessage_forReply(self,messageType:str,message:str = "", contentURL:str = "",audioDuration:int= 1000) -> None:
+
+        match messageType.lower():
+            case "text":
+                self.body["message"] = ({
+                    [
+                {
+                    "type":"text",
+                    "text":message
+                }   ]})
+
+            case "audio":
+                self.body["message"] = ({
+                    "type": "audio",
+                    "originalContentUrl": contentURL,
+                    "duration": audioDuration
+                    })
+
+
+    def sendMessage(self) -> str:
+        response = req.post(url=replyUrl,headers=self.header,json=self.body)
+        return response.text
+
+if __name__ == "__main__":
+    line:object = LineAPI()
+    line.addMessage_forReply(messageType="Audio",contentURL="hello",audioDuration=3000)
